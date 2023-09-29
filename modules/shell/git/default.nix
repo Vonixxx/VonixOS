631d72157711c4edd5995814daf686791c11b866
Vonixxx <vonixxxwork@tuta.io>
2023-09-29 13:23:03 +0200

    changed some settings

--- modules/shell/git/default.nix
+++ modules/shell/git/default.nix
@@ -8,16 +8,10 @@
 {
  home-manager.users.${vars.user} = {
    programs.git = {
-     ##########
-     # Enable #
-     ##########
      enable = true;
    };
-   # programs.git-credential-oauth = {
-   #   ##########
-   #   # Enable #
-   #   ##########
-   #   enable = true;
-   # };
+   programs.git-credential-oauth = {
+       enable = true;
+   };
  };
 }
