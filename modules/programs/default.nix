7eecc5481288a74361e06d53802a6b83c8b7fb7b
Vonixxx <vonixxxwork@tuta.io>
2023-10-22 12:31:16 +0200

    added firefox and nur

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -7,10 +7,6 @@
  ./feh/default.nix
  ./mpv/default.nix
  ./btop/default.nix
-<<<<<<< HEAD
  ./firefox/default.nix
-=======
- ./chromium/default.nix
->>>>>>> 93f6bac3b9f355cec3fad371b67edcde7f8bdff7
  ./librewolf/default.nix
 ]
