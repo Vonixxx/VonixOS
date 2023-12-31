c21a6814f3a0be8e8a7afca188b63dae959fc9da
Vonixxx <vonixxxwork@tuta.io>
2023-12-31 15:35:03 +0100

    testing

--- system/hosts/gnome/characteristics/users/default.nix
+++ system/hosts/gnome/characteristics/users/default.nix
@@ -3,14 +3,14 @@
 ######################
 # User Configuration #
 ######################
-{ vars, unknown-vars, ... }:
+{ vars, ... }:
 
 {
  users.users.${vars.user} = {
-   uid          = 1000;
-   isNormalUser = true;
-   home         = "/home/bubinka";
-   password     = "${unknown-vars.password}";
-   extraGroups  = [ "audio" "video" "wheel" "networkmanager" ];
+   uid            = 1000;
+   isNormalUser   = true;
+   home           = "/home/bubinka";
+   extraGroups    = [ "audio" "video" "wheel" "networkmanager" ];
+   hashedPassword = "$y$j9T$YQnrV6FSbngHwY4Y/xCR7/$b5I3pMtjPHb8YQdjXwuEZLFna9Nj2h7eT6uRP4P7n.4";
  };
 }
--- system/hosts/sway/characteristics/users/default.nix
+++ system/hosts/sway/characteristics/users/default.nix
@@ -3,14 +3,14 @@
 ######################
 # User Configuration #
 ######################
-{ vars, unknown-vars, ... }:
+{ vars, ... }:
 
 {
  users.users.${vars.user} = {
-   uid          = 1000;
-   isNormalUser = true;
-   home         = "/home/vonix";
-   password     = "${unknown-vars.password}";
-   extraGroups  = [ "audio" "video" "wheel" "libvirtd" "networkmanager" ];
+   uid            = 1000;
+   isNormalUser   = true;
+   home           = "/home/vonix";
+   extraGroups    = [ "audio" "video" "wheel" "libvirtd" "networkmanager" ];
+   hashedPassword = "$y$j9T$eDooCqRrtgj05orlhUujQ1$RDV9aOlJZkKZI6wtkpR.YD00ELzIlNZbDWY8IiDIxfB";
  };
 }
