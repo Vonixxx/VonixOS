235e8b69d7d43ccb7caa66005accaf9d7f97b781
Vonixxx <vonixxxwork@tuta.io>
2024-01-02 20:22:26 +0100

    testing

--- system/hosts/hyprland/characteristics/users/default.nix
+++ system/hosts/hyprland/characteristics/users/default.nix
@@ -9,7 +9,7 @@
  users.users.${vars.user} = {
    uid            = 1000;
    isNormalUser   = true;
-   home           = "/home/voyager";
+   home           = "/home/vonix";
    extraGroups    = [ "audio" "video" "wheel" "libvirtd" "networkmanager" ];
    hashedPassword = "$y$j9T$eDooCqRrtgj05orlhUujQ1$RDV9aOlJZkKZI6wtkpR.YD00ELzIlNZbDWY8IiDIxfB";
  };
