966f27182cceec2aa6f6c3661c700a067ab1e184
Vonixxx <vonixxxwork@tuta.io>
2024-01-13 20:18:22 +0100

    testing

--- modules/environments/gnome/default.nix
+++ modules/environments/gnome/default.nix
@@ -24,6 +24,10 @@
 
    home-manager.users.${vars.user} = {
      dconf.settings = {
+       "org/gnome/desktop/session" = {
+         idle-delay = 0;
+       };
+
        "org/gnome/system/location" = {
          enabled = true;
        };
@@ -38,18 +42,9 @@
          remove-old-temp-files  = true;
        };
 
-       "org/gnome/desktop/interface" = {
-         enable-hot-corners = false;
-         color-scheme       = "prefer-dark";
-       };
-
-       "org/gnome/desktop/session" = {
-         idle-delay = 0;
-       };
-
        "org/gnome/shell/extensions/arcmenu" = {
          distro-icon                  = 8;
-         custom-menu-button-icon-size = 35.0;
+         custom-menu-button-icon-size = 50.0;
          hide-overview-on-startup     = true;
          menu-layout                  = "Budgie";
        };
@@ -64,6 +59,21 @@
          sleep-inactive-battery-type = "nothing";
          sleep-inactive-ac-type      = "nothing";
          power-button-action         = "interactive";
+       };
+
+       "org/gnome/desktop/interface" = {
+         enable-hot-corners  = false;
+         font-antialiasing   = "rgba";
+         font-hinting        = "full";
+         color-scheme        = "prefer-dark";
+         font-name           = "Liberation Serif 12";
+         document-font-name  = "Liberation Serif 12";
+         monospace-font-name = "Liberation Mono 12";
+         titlebar-font       = "Liberation Serif Bold 12";
+       };
+
+       "org/gnome/desktop/wm/preferences" = {
+         button-layout = "appmenu:minimize,maximize,close";
        };
 
        "org/gnome/shell" = {
@@ -90,26 +100,27 @@
        };
 
        "org/gnome/shell/extensions/dash-to-panel" = {
-         appicon-margin      = 4;
-         appicon-padding     = 6;
-         dot-position        = "TOP";
-         dot-style-focused   = "METRO";
-         dot-style-unfocused = "DASHES";
-         panel-sizes         = "{\"0\":48}";
-         panel-lengths       = "{\"0\":100}";
-         panel-positions     = "{\"0\":\"BOTTOM\"}";
+         appicon-margin        = 4;
+         appicon-padding       = 8;
+         animate-appicon-hover = true;
+         dot-position          = "TOP";
+         dot-style-focused     = "METRO";
+         dot-style-unfocused   = "DASHES";
+         panel-sizes           = "{\"0\":56}";
+         panel-lengths         = "{\"0\":100}";
+         panel-positions       = "{\"0\":\"BOTTOM\"}";
 
          panel-element-positions = "{\"0\":[
+             {\"element\":\"leftBox\",\"visible\":true,\"position\":\"stackedTL\"},
+             {\"element\":\"taskbar\",\"visible\":true,\"position\":\"stackedTL\"},
+             {\"element\":\"systemMenu\",\"visible\":true,\"position\":\"stackedBR\"},
+             {\"element\":\"dateMenu\",\"visible\":true,\"position\":\"stackedBR\"}
+
              {\"element\":\"rightBox\",\"visible\":false,\"position\":\"stackedTL\"},
              {\"element\":\"centerBox\",\"visible\":false,\"position\":\"stackedTL\"},
              {\"element\":\"desktopButton\",\"visible\":false,\"position\":\"stackedTL\"},
              {\"element\":\"showAppsButton\",\"visible\":false,\"position\":\"stackedTL\"},
              {\"element\":\"activitiesButton\",\"visible\":false,\"position\":\"stackedTL\"},
-
-             {\"element\":\"leftBox\",\"visible\":true,\"position\":\"stackedTL\"},
-             {\"element\":\"taskbar\",\"visible\":true,\"position\":\"stackedTL\"},
-             {\"element\":\"systemMenu\",\"visible\":true,\"position\":\"stackedTR\"},
-             {\"element\":\"dateMenu\",\"visible\":true,\"position\":\"stackedTR\"}
          ]}";
        };
      };
