876c007017e87bbac4f0427f731b78b608e563fd
Vonixxx <vonixxxwork@tuta.io>
2024-01-13 18:16:09 +0100

    testing

--- modules/environments/gnome/default.nix
+++ modules/environments/gnome/default.nix
@@ -24,28 +24,63 @@
 
    home-manager.users.${vars.user} = {
      dconf.settings = {
-       "org/gnome/shell" = {
-         disable-user-extensions = false;
-         disabled-extensions = [ 
-           ""
-         ];
-         enabled-extensions = [ 
-           "space-bar@luchrioh"
-           "arcmenu@arcmenu.com" 
-           # "dash-to-panel@jderose9.github.com"
-           "burn-my-windows@schneegans.github.com"
-           "user-theme@gnome-shell-extensions.gcapax.github.com"
-         ];
+       "org/gnome/system/location" = {
+         enabled = true;
        };
+
+       "org/gnome/desktop/datetime" = {
+         automatic-timezone = true;
+       };
+
+       "org/gnome/desktop/privacy" = {
+         recent-files-max-age   = 30;
+         remove-old-trash-files = true;
+         remove-old-temp-files  = true;
+       };
+
+       "org/gnome/desktop/interface" = {
+         enable-hot-corners = false;
+         color-scheme       = "prefer-dark";
+       };
+
+       "org/gnome/desktop/session" = {
+         idle-delay = hm.gvariant.mkUint32 0;
+       };
+
        "org/gnome/shell/extensions/arcmenu" = {
          distro-icon                  = 8;
          custom-menu-button-icon-size = 35;
          hide-overview-on-startup     = true;
          menu-layout                  = "Budgie";
        };
-       # "org/gnome/shell/extensions/dash-to-panel" = {
-       #   panel-positions = "0:TOP";
-       # };
+
+       "org/gnome/settings-daemon/plugins/power" = {
+         sleep-inactive-battery-type = "nothing";
+         sleep-inactive-ac-type      = "nothing";
+         power-button-action         = "interactive";
+       };
+
+       "org/gnome/shell" = {
+         disable-user-extensions = false;
+
+         disabled-extensions = [ 
+           ""
+         ];
+
+         enabled-extensions = [ 
+           "space-bar@luchrioh"
+           "arcmenu@arcmenu.com" 
+           "dash-to-panel@jderose9.github.com"
+           "burn-my-windows@schneegans.github.com"
+           "user-theme@gnome-shell-extensions.gcapax.github.com"
+         ];
+       };
+
+       "org/gnome/settings-daemon/plugins/color" = {
+         night-light-enabled            = true;
+         night-light-schedule-automatic = true;
+         night-light-temperature        = hm.gvariant.mkUint32 1700;
+       };
      };
    };
  };
