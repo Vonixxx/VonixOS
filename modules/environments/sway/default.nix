225b6d713ae8af39c887e4594fd645d9eafdd243
Vonixxx <vonixxxwork@tuta.io>
2024-01-15 20:08:27 +0100

    testing

--- modules/environments/sway/default.nix
+++ modules/environments/sway/default.nix
@@ -73,7 +73,7 @@
            "${modifier}+t" = "exec ~/.nix-profile/bin/foot";
            "${modifier}+d" = "exec ~/.nix-profile/bin/fuzzel";
            "${modifier}+b" = "exec ~/.nix-profile/bin/firefox";
-           "${modifier}+y" = "exec ${pkgs.freetube}/bin/freetube";
+           "${modifier}+y" = "exec ${stable.freetube}/bin/freetube";
            ######################################
            # Switching Containers -> Workspaces #
            ######################################
