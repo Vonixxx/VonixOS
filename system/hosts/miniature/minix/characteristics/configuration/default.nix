1667dcadf9b3942ddca45fb5369c9577871d4abb
Vonixxx <vonixxxwork@tuta.io>
2024-01-19 16:34:08 +0100

    testing

--- system/hosts/miniature/minix/characteristics/configuration/default.nix
+++ system/hosts/miniature/minix/characteristics/configuration/default.nix
@@ -10,9 +10,9 @@
  services.xserver.layout = "cz"; 
 
  hardware.firmware = [
-   pkgs.runCommand "brcm-firmware" { } ''
+   (pkgs.runCommand "brcm-firmware" { } ''
       mkdir -p $out/lib/firmware/
       cp -r ${./brcm} "$out"/lib/firmware/
-   ''
+   '')
  ];
 }
