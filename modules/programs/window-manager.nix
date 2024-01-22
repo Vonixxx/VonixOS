b93dd6f11fbce7c98ddd18148cae955c8a682d76
Vonixxx <vonixxxwork@tuta.io>
2024-01-22 14:40:54 +0100

    testing

--- modules/programs/window-manager.nix
+++ modules/programs/window-manager.nix
@@ -8,6 +8,7 @@
  ./window-manager/mako/default.nix
  ./window-manager/wofi/default.nix
  ./window-manager/cursor/default.nix
+ ./window-manager/waybar/default.nix
  ./window-manager/sioyek/default.nix
  ./window-manager/wpaperd/default.nix
  ./window-manager/wlsunset/default.nix
