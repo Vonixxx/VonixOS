777917b3479efe5b72766aef148f7d3eaa6d88c3
Vonixxx <vonixxxwork@tuta.io>
2024-01-27 17:46:47 +0100

    testing

--- system/hosts/laptops/hp/f.jarka/characteristics/hardware/default.nix
+++ system/hosts/laptops/hp/f.jarka/characteristics/hardware/default.nix
@@ -14,18 +14,18 @@
    "boot" = { 
      fsType     = "vfat";
      mountPoint = "/boot";
-     device     = "/dev/disk/by-uuid/3C5B-4983";
+     device     = "/dev/disk/by-uuid/F673-D531";
    };
 
    "root" = { 
      mountPoint = "/";
      fsType     = "ext4";
-     device     = "/dev/disk/by-uuid/c101aa16-9be1-482c-b349-2d2a1cf54644";
+     device     = "/dev/disk/by-uuid/a34b2349-0770-4bf8-8d7e-ee6b6ae24eac";
    };
  };
 
  boot = { 
    kernelModules        = [ "i915" "kvm-intel" ];
-   initrd.kernelModules = [ "vmd" "ahci" "ext4" "nvme" "sd_mod" "xhci_pci" "usb_storage" ];
+   initrd.kernelModules = [ "vmd" "ahci" "ext4" "nvme" "sd_mod" "sr_mod" "xhci_pci" "usb_storage" "rtsx_usb_sdmmc" ];
  };
 }
