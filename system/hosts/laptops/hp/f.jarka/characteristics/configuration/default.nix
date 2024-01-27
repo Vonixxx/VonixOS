e938de8db7d7e5b4b8187d36131fd85c3dc54ffe
Vonixxx <vonixxxwork@tuta.io>
2024-01-27 18:15:26 +0100

    testing

--- system/hosts/laptops/hp/f.jarka/characteristics/configuration/default.nix
+++ system/hosts/laptops/hp/f.jarka/characteristics/configuration/default.nix
@@ -6,7 +6,17 @@
 { ... }:
 
 {
- gnome.enable            = true;
- services.xserver.layout = "cz"; 
- i18n.defaultLocale      = "cs_CZ.UTF-8";
+ gnome.enable       = true;
+ i18n.defaultLocale = "cs_CZ.UTF-8";
+
+ services = {
+   xserver.layout = "cz"; 
+   printing = {
+     enable          = true;
+     browsing        = true;
+     openFirewall    = true;
+     defaultShared   = true;
+     startWhenNeeded = false;
+   };
+ };
 }
