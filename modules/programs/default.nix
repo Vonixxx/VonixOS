8c6d8629223218807830d9bfc5cf2dcccba2a07e
Vonixxx <vonixxxwork@tuta.io>
2024-01-28 16:41:10 +0100

    testing

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -29,38 +29,38 @@
  };
 
  config = mkIf (config.terminal.enable) {
-   [
-    ./terminal/bat/default.nix
-    ./terminal/git/default.nix
-    ./terminal/mpv/default.nix
-    ./terminal/lsd/default.nix
-    ./terminal/zsh/default.nix
-    ./terminal/foot/default.nix
-    ./terminal/helix/default.nix
-    ./terminal/sioyek/default.nix
-   ]
+   imports = [
+     ./terminal/bat/default.nix
+     ./terminal/git/default.nix
+     ./terminal/mpv/default.nix
+     ./terminal/lsd/default.nix
+     ./terminal/zsh/default.nix
+     ./terminal/foot/default.nix
+     ./terminal/helix/default.nix
+     ./terminal/sioyek/default.nix
+   ];
  };
 
  config = mkIf (config.generic.enable) {
-   [
-    ./generic/firefox/default.nix
-   ]
+   imports = [
+     ./generic/firefox/default.nix
+   ];
  };
 
  config = mkIf (config.recording.enable) {
-   [
-    ./recording/kdenlive-obs/default.nix
-   ]
+   imports = [
+     ./recording/kdenlive-obs/default.nix
+   ];
  };
 
  config = mkIf (config.window-manager.enable) {
-   [
-    ./window-manager/mako/default.nix
-    ./window-manager/wofi/default.nix
-    ./window-manager/cursor/default.nix
-    ./window-manager/waybar/default.nix
-    ./window-manager/wpaperd/default.nix
-    ./window-manager/gammastep/default.nix
-   ]
+   imports = [
+     ./window-manager/mako/default.nix
+     ./window-manager/wofi/default.nix
+     ./window-manager/cursor/default.nix
+     ./window-manager/waybar/default.nix
+     ./window-manager/wpaperd/default.nix
+     ./window-manager/gammastep/default.nix
+   ];
  };
 }
