3a02a863db89cbb838057593dcdcc844a34df593
Vonixxx <vonixxxwork@tuta.io>
2024-01-28 17:28:52 +0100

    testing

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -28,39 +28,41 @@
    };
  };
 
- config = mkIf (config.terminal.enable) {
-   imports = [
-     ./terminal/bat/default.nix
-     ./terminal/git/default.nix
-     ./terminal/mpv/default.nix
-     ./terminal/lsd/default.nix
-     ./terminal/zsh/default.nix
-     ./terminal/foot/default.nix
-     ./terminal/helix/default.nix
-     ./terminal/sioyek/default.nix
-   ];
- };
+ config = mkMerge [ 
+   (mkIf (config.terminal.enable) {
+     imports = [
+       ./terminal/bat/default.nix
+       ./terminal/git/default.nix
+       ./terminal/lsd/default.nix
+       ./terminal/mpv/default.nix
+       ./terminal/zsh/default.nix
+       ./terminal/foot/default.nix
+       ./terminal/helix/default.nix
+       ./terminal/sioyek/default.nix
+     ];
+   })
 
- config = mkIf (config.generic.enable) {
-   imports = [
-     ./generic/firefox/default.nix
-   ];
- };
+   (mkIf (config.generic.enable) {
+     imports = [
+       ./generic/firefox/default.nix
+     ];
+   })
 
- config = mkIf (config.recording.enable) {
-   imports = [
-     ./recording/kdenlive-obs/default.nix
-   ];
- };
+   (mkIf (config.recording.enable) {
+     imports = [
+       ./recording/kdenlive-obs/default.nix
+     ];
+   })
 
- config = mkIf (config.window-manager.enable) {
-   imports = [
-     ./window-manager/mako/default.nix
-     ./window-manager/wofi/default.nix
-     ./window-manager/cursor/default.nix
-     ./window-manager/waybar/default.nix
-     ./window-manager/wpaperd/default.nix
-     ./window-manager/gammastep/default.nix
-   ];
- };
+   (mkIf (config.terminal.enable) {
+     imports = [
+       ./window-manager/wofi/default.nix
+       ./window-manager/mako/default.nix
+       ./window-manager/cursor/default.nix
+       ./window-manager/waybar/default.nix
+       ./window-manager/wpaperd/default.nix
+       ./window-manager/gammastep/default.nix
+     ];
+   })
+ ];
 }
