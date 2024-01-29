6a956fdbabc29c2fd15f7541e25132247c748992
Vonixxx <vonixxxwork@tuta.io>
2024-01-29 21:39:57 +0100

    testing

--- hosts/laptops/lenovo/u.luca/characteristics/default.nix
+++ hosts/laptops/lenovo/u.luca/characteristics/default.nix
@@ -3,10 +3,14 @@
 #################################
 # System-Specific Settings List #
 #################################
-[
+{ ... }:
+
+{
+imports = [
  ./users/default.nix
  ./hardware/default.nix
  ./packages/default.nix
  ./bookmarks/default.nix
  ./configuration/default.nix
-]
+];
+}
