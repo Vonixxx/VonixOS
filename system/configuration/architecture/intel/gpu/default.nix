958cc0f3d1bdb1e2d3e3301f544301b1ea2d4324
Vonixxx <vonixxxwork@tuta.io>
2024-02-08 11:45:34 +0100

    testing

--- system/configuration/architecture/intel/gpu/default.nix
+++ system/configuration/architecture/intel/gpu/default.nix
@@ -3,18 +3,13 @@
 ###########################
 # Intel GPU Configuration #
 ###########################
-{ lib
-, pkgs
-, config 
-, ...
-}: 
+{ lib, pkgs, config, ... }: 
 
 with lib;
 with pkgs;
-with config;
 
 {
- config = mkIf (intelgpu.enable) {
+ config = mkIf (config.intelgpu.enable) {
    boot.kernelModules            = [ "i915" ];
    hardware.opengl.extraPackages = [ intel-media-driver ];
  };
