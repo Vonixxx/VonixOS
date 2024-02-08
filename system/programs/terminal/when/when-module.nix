9dcd61015382655d96b5b30e2554d4601ad5fffe
Vonixxx <vonixxxwork@tuta.io>
2024-02-08 21:36:43 +0100

    testing

--- system/programs/terminal/when/when-module.nix
+++ system/programs/terminal/when/when-module.nix
@@ -1,67 +1,70 @@
+###########
+# VonixOS #
+######################
+# When Configuration #
+######################
+{ ... }: 
+
 {
-home-manager = {
-    lib
-  , config
-  , unstable
-  , ... 
-}:
-   
-with lib;
-with types;
-with unstable;
-with maintainers;
+ home-manager = { lib, config, unstable, ... }:
 
-let 
- cfg = config.programs.when; 
-in 
-{
- options.programs.when = {
-   meta.maintainers = [ vonixxx ];
-   enable           = mkEnableOption "when";
-   package          = mkPackageOption unstable "when" { };
+  with lib;
+  with types;
+  with unstable;
+  with maintainers;
 
-   preferences = mkOption {
-     default = { };
-     type    = attrsOf (oneOf [ int str ]);
+  let 
+   cfg = config.programs.when; 
+  in 
 
-     example = literalExpression ''
-        {
-         styled_output = 1; # 1/0 = True/False
-         future        = 14;
-         language      = "en";
-        }
-     '';
-     description = ''
-        Configuration for .when/preferences
-         See <https://www.lightandmatter.com/when/when.html>
-         for options.
-     '';
+  {
+   options.programs.when = {
+     meta.maintainers = [ vonixxx ];
+     enable           = mkEnableOption "when";
+     package          = mkPackageOption unstable "when" { };
+  
+     preferences = mkOption {
+       default = { };
+       type    = attrsOf (oneOf [ int str ]);
+  
+       example = literalExpression ''
+          {
+           styled_output = 1; # 1/0 = True/False
+           future        = 14;
+           language      = "en";
+          }
+       '';
+       description = ''
+          Configuration for .when/preferences
+           See <https://www.lightandmatter.com/when/when.html>
+           for options.
+       '';
+     };
+  
+     calendar = mkOption {
+       default = "";
+       type    = lines;
+  
+       description = ''
+          Configuration for .when/calendar
+           See <https://www.lightandmatter.com/when/when.html>
+           for options.
+       '';
+     };
    };
-
-   calendar = mkOption {
-     default = "";
-     type    = lines;
-
-     description = ''
-        Configuration for .when/calendar
-         See <https://www.lightandmatter.com/when/when.html>
-         for options.
-     '';
+  
+   config = mkIf cfg.enable {
+     home.packages = [ cfg.package ];
+  
+     xdg.configFile.".when/calendar" = mkIf (cfg.calendar != null) { 
+       text = writeText ".when/calendar" cfg.calendar;
+     };
+  
+     xdg.configFile.".when/preferences" = mkIf (cfg.preferences != { }) { 
+       text = writeText ".when/preferences" cfg.preferences;
+     };
+  
+     assertions = [ (hm.assertions.assertPlatform "programs.when" unstable platforms.linux) ];
    };
  };
-
- config = mkIf cfg.enable {
-   home.packages = [ cfg.package ];
-
-   xdg.configFile.".when/calendar" = mkIf (cfg.calendar != null) { 
-     text = writeText ".when/calendar" cfg.calendar;
-   };
-
-   xdg.configFile.".when/preferences" = mkIf (cfg.preferences != { }) { 
-     text = writeText ".when/preferences" cfg.preferences;
-   };
-
-   assertions = [ (hm.assertions.assertPlatform "programs.when" unstable platforms.linux) ];
- };
-};
 }
