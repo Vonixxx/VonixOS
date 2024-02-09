47003eb066ac1e91438e0e32ace59c3055065eb2
Vonixxx <vonixxxwork@tuta.io>
2024-02-09 20:53:17 +0100

    testing

--- system/programs/terminal/when/when-module.nix
+++ system/programs/terminal/when/when-module.nix
@@ -3,72 +3,70 @@
 ######################
 # When Configuration #
 ######################
+{ lib, config, unstable, ... }:
+
+with lib;
+with types;
+with unstable;
+with maintainers;
+
+let 
+ cfg = config.programs.when; 
+in 
+
 {
- home-manager = { lib, config, unstable, ... }:
+ options.programs.when = {
+   meta.maintainers = [ vonixxx ];
+   enable           = mkEnableOption "when";
+   package          = mkPackageOption unstable "when" { };
 
-  with lib;
-  with types;
-  with unstable;
-  with maintainers;
+   preferences = mkOption {
+     default = { };
+     type    = attrsOf (oneOf [ int str ]);
 
-  let 
-   cfg = config.programs.when; 
-  in 
-
-  {
-   options.programs.when = {
-     meta.maintainers = [ vonixxx ];
-     enable           = mkEnableOption "when";
-     package          = mkPackageOption unstable "when" { };
-  
-     preferences = mkOption {
-       default = { };
-       type    = attrsOf (oneOf [ int str ]);
-  
-       example = literalExpression ''
-          {
-           styled_output = 1; # 1/0 = True/False
-           future        = 14;
-           language      = "en";
-          }
-       '';
-       description = ''
-          Configuration for .when/preferences
-           See <https://www.lightandmatter.com/when/when.html>
-           for options.
-       '';
-     };
-  
-     calendar = mkOption {
-       default = "";
-       type    = lines;
-  
-       example = literalExpression ''
-          {
-           * dec 25     , Christmas
-           m=dec & d=25 , Christmas
-           w=sat & b=1  , Rehearse with band.
-           2003 feb 3   , Fly to Stockholm to accept Nobel Prize.
-          }
-       '';
-       description = ''
-          Configuration for .when/calendar
-           See <https://www.lightandmatter.com/when/when.html>
-           for options.
-       '';
-     };
+     example = literalExpression ''
+        {
+         styled_output = 1; # 1/0 = True/False
+         future        = 14;
+         language      = "en";
+        }
+     '';
+     description = ''
+        Configuration for .when/preferences
+         See <https://www.lightandmatter.com/when/when.html>
+         for options.
+     '';
    };
-  
-   config = mkIf cfg.enable {
-     home.packages = [ cfg.package ];
-  
-     xdg.configFile.".when/calendar" = mkIf (cfg.calendar != null) { 
-       text = writeText ".when/calendar" cfg.calendar;
-     };
-  
-     xdg.configFile.".when/preferences" = mkIf (cfg.preferences != { }) { 
-       text = writeText ".when/preferences" cfg.preferences;
-     };
+
+   calendar = mkOption {
+     default = "";
+     type    = lines;
+
+     example = literalExpression ''
+        {
+         * dec 25     , Christmas
+         m=dec & d=25 , Christmas
+         w=sat & b=1  , Rehearse with band.
+         2003 feb 3   , Fly to Stockholm to accept Nobel Prize.
+        }
+     '';
+     description = ''
+        Configuration for .when/calendar
+         See <https://www.lightandmatter.com/when/when.html>
+         for options.
+     '';
    };
  };
-}
+
+ config = mkIf cfg.enable {
+   home.packages = [ cfg.package ];
+
+   xdg.configFile.".when/calendar" = mkIf (cfg.calendar != null) { 
+     text = writeText ".when/calendar" cfg.calendar;
+   };
+
+   xdg.configFile.".when/preferences" = mkIf (cfg.preferences != { }) { 
+     text = writeText ".when/preferences" cfg.preferences;
+   };
+ };
+;
