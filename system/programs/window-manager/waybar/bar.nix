b671a41306c902d2b356131693687fc1b9981955
Vonixxx <vonixxxwork@tuta.io>
2024-02-21 17:54:38 +0100

    testing

--- system/programs/window-manager/waybar/bar.nix
+++ system/programs/window-manager/waybar/bar.nix
@@ -113,13 +113,13 @@ with lib;
         "cpu" = {
           interval = 10;
           tooltip  = false;
-          format   = "<big>󰻠</big> <span rise='1700'>[{usage}%]</span>";
+          format   = "<span rise='100'><big>󰻠</big></span> <span rise='1700'>[{usage}%]</span>";
         };
 
         "memory" = {
           interval = 10;
           tooltip  = false;
-          format   = "<big>󰍛</big> <span rise='1700'>[{percentage}%]</span>";
+          format   = "<span rise='100'><big>󰍛</big></span> <span rise='1700'>[{percentage}%]</span>";
         };
      }];
    };
