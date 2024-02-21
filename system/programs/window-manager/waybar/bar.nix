16acf48f1068aa2dd0d03ca98f71bb86a8d70ff3
Vonixxx <vonixxxwork@tuta.io>
2024-02-21 17:24:12 +0100

    testing

--- system/programs/window-manager/waybar/bar.nix
+++ system/programs/window-manager/waybar/bar.nix
@@ -43,13 +43,13 @@ with lib;
         "cpu" = {
           interval = 10;
           tooltip  = false;
-          format   = "<big>󰻠</big> <span rise='2000'>[{usage}%]</span>";
+          format   = "<big>󰻠</big> <span rise='1500'>[{usage}%]</span>";
         };
 
         "memory" = {
           interval = 10;
           tooltip  = false;
-          format   = "<big>󰍛</big> <span rise='3000'>[{percentage}%]</span>";
+          format   = "<big>󰍛</big> <span rise='1250'>[{percentage}%]</span>";
         };
 
         "custom/reboot" = {
